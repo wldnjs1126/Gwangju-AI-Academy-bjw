62a3d4257df15323b37a90b0ec0ddf040e185505
admin <admin@admin.com>
2026-07-03 14:08:41 +0900

    commit

--- 09.RAG-랭체인-랭그래프/01-기본-설정-및-구동/03-LLM-객체비교_및_생성.py
+++ 09.RAG-랭체인-랭그래프/01-기본-설정-및-구동/03-LLM-객체비교_및_생성.py
@@ -7,13 +7,24 @@ load_dotenv()
 # LLM 객체 생성 방법 3가지 (Large Language Model)
 
 # 첫번째 방법 : OpenAI()로 객체 생성 (간단하게 invoke 함수를 사용해서 할 수 있음, str 형태로 답변을 줌)
-# llm = OpenAI()
-# result = llm.invoke("대한민국 수도는?")
-# print(type(result))
-# print(result)
+llm = OpenAI()
+result = llm.invoke("대한민국 수도는?")
+print(type(result))
+print(result)
 
 # 두번째 방법 : OpenAI()로 객체 생성 (간단하게 보면 챗지피티를 가지고 오는 것)
 llm = ChatOpenAI()
 result = llm.invoke("대한민국 수도는?")
 print(result.content)
 print(result)
+
+# 세번째 방법
+from langchain.chat_models import init_chat_model
+import os
+
+model_name = os.getenv("LLM_AI_MODEL")
+llm = init_chat_model(model_name)
+
+respose = llm.invoke("오늘 날씨 어때?")
+print(respose.content)
+print(respose)
