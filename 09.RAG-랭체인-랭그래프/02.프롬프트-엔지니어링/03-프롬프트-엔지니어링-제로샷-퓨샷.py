bb038385800408e3d891a9cae82af11afd4c78c5
admin <admin@admin.com>
2026-07-03 15:46:03 +0900

    commit

--- 09.RAG-랭체인-랭그래프/02.프롬프트-엔지니어링/03-프롬프트-엔지니어링-제로샷-퓨샷.py
+++ 09.RAG-랭체인-랭그래프/02.프롬프트-엔지니어링/03-프롬프트-엔지니어링-제로샷-퓨샷.py
@@ -14,54 +14,13 @@ from langchain.chat_models import init_chat_model
 from langchain_core.prompts import ChatPromptTemplate
 from openai import OpenAI
 
-load_dotenv()
-
-from openai import OpenAI
-
-client = OpenAI()
+# 제로샷 :  예시가 없음 => 모델이 바로 답변 (AI가 기준없이 바로 답변 생성)
 
 prompt = """
-당신은 다음 특징을 가진 AI입니다.
+다음 문장을 영어로 번역하세요.
 
-[Persona]
-- 10년차 데이터 사이언티스트
-- 삼성전자 AI 연구원 출신
-- 친절하고 쉽게 설명하는 스타일
-
-[Task]
-머신러닝이 무엇인지 설명하세요.
-
-[Constraint]
-- 5줄 이내
-- 비유 1개 포함
-- 초보자 대상
+나는 오늘 학교에 갔다.
 """
-
-# 여러 캐릭터 지정이 가능 
-prompt = """
-다음 질문에 대해 서로 다른 Role로 답변하세요.
-
-질문: AI란 무엇인가?
-
-Role 1: AI 연구원
-Role 2: 초등학교 선생님
-Role 3: 비유를 많이 쓰는 유튜버
-
-각 Role별로 답변을 구분해서 작성하세요.
-"""
-
-# 역할하고 Tone 조합
-prompt = """
-당신은 친절한 스타트업 CTO입니다.
-
-스타트업에서 AI를 도입해야 하는 이유를 설명하세요.
-
-조건:
-- 말투: 친근하고 현실적
-- 예시 포함
-- 투자자 설득용 느낌
-"""
-
 import sys
 from pathlib import Path
 import os
@@ -75,4 +34,4 @@ llm = init_custom_llm()
 respose = llm.invoke(prompt)
 
 print(respose.content)
-# print(respose)
+
