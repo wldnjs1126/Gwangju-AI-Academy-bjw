6aa1c36660434178749d083c58988c98adb7c191
admin <admin@admin.com>
2026-07-06 11:48:27 +0900

    commit

--- 09.RAG-랭체인-랭그래프/02.프롬프트-엔지니어링/06-프롬프트-엔지니어링-메세지_객체의_이해.py
+++ 09.RAG-랭체인-랭그래프/02.프롬프트-엔지니어링/06-프롬프트-엔지니어링-메세지_객체의_이해.py
@@ -10,12 +10,54 @@ import os
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 from llm_loader import init_custom_llm
 from langchain_core.prompts import PromptTemplate
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage, ToolMessage
+# 잘 모르겠으면 모든걸 가져오겠다는 뜻의 " * " 을 사용해도 됨 
 
 # 메세지란?
+# System
+# 당신은 친절한 AI입니다.
 
+# Human
+# AI 란 무엇인가요?
 
+# AI
+# AI는 인공지능입니다.
+
+# Human
+# 예제를 보여주세요
+
+# 한 줄의 대화 = message 객체 하나로 관리
+# 결론) 전체 대화 = Message List
+
+# 1. 시스템 메세지 만들기
+system = SystemMessage(content="당신은 친절한 분석가입니다.")
+print(system)
+print(system.content)
+
+# 2. AI 메세지 만들기
+ai = AIMessage(
+    content="AI는 사람처럼 학습하는 기술입니다."
+)
+
+print(ai.content)
+
+# 3. 유저 질문
+human = HumanMessage(content="클래스에 대해 설명해줘")
+print(human.content)
+
+# 4. Tool 함수
+tool = ToolMessage(
+    content = "5",
+    tool_call_id = "call_1234"
+)
+
+messages = [
+    system,
+    ai,
+    human
+]
 
 llm = init_custom_llm()
-respose = llm.invoke(result)
+respose = llm.invoke(messages)
 
 print(respose.content)
