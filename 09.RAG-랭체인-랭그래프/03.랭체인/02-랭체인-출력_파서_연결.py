3ec1b531e38d1c140a0f0fe3a323e3dbd0c21a48
admin <admin@admin.com>
2026-07-07 13:21:10 +0900

    commit

--- 09.RAG-랭체인-랭그래프/03.랭체인/02-랭체인-출력_파서_연결.py
+++ 09.RAG-랭체인-랭그래프/03.랭체인/02-랭체인-출력_파서_연결.py
@@ -14,36 +14,36 @@ from langchain_core.output_parsers import StrOutputParser, JsonOutputParser, XML
 
 llm = init_custom_llm()
 
-prompt = ChatPromptTemplate.from_template(
-"""
-JSON 출력  
-{{
-    "제목" : "",
-    "내용" : ""
-}}
+# prompt = ChatPromptTemplate.from_template(
+# """
+# JSON 출력  
+# {{
+#     "제목" : "",
+#     "내용" : ""
+# }}
 
-질문 :
-{topic}
-"""
-)
+# 질문 :
+# {topic}
+# """
+# )
 
-# msg = prompt.invoke({
-#     "topic" : "AI Agent"
+# # msg = prompt.invoke({
+# #     "topic" : "AI Agent"
+# # })
+
+# # print(msg)
+
+# # 출력이 스트링으로 뽑아서 
+# output_parser = StrOutputParser() # => 문자열만 반환 
+# # output_parser = JsonOutputParser()  # => Json 문자열을 파이썬 객체로 변환
+
+# chain = prompt | llm | output_parser
+
+# result = chain.invoke({
+#     "topic" : "AI의 미래는?"
 # })
 
-# print(msg)
-
-# 출력이 스트링으로 뽑아서 
-output_parser = StrOutputParser() # => 문자열만 반환 
-# output_parser = JsonOutputParser()  # => Json 문자열을 파이썬 객체로 변환
-
-chain = prompt | llm | output_parser
-
-result = chain.invoke({
-    "topic" : "AI의 미래는?"
-})
-
-print(result)  # output_parser을 붙히지 않았으면 뒤에 .content를 붙혀서 사용했어야 함
+# print(result)  # output_parser을 붙히지 않았으면 뒤에 .content를 붙혀서 사용했어야 함
 
 
 
@@ -55,21 +55,21 @@ print(result)  # output_parser을 붙히지 않았으면 뒤에 .content를 붙�
 # print(result.content)
 
 
-# prompt = ChatPromptTemplate.from_template("""
-# 아래 XML 형식으로만 답하세요.
+prompt = ChatPromptTemplate.from_template("""
+아래 XML 형식으로만 답하세요.
 
 
-# 질문:
-# {topic}
-# """)
+질문:
+{topic}
+""")
 
-# output_parser = XMLOutputParser()
+output_parser = XMLOutputParser()
 
 
-# chain = prompt | llm | output_parser
+chain = prompt | llm | output_parser
 
-# result = chain.invoke({
-#     "topic":"AI의 미래는?"
-# })
+result = chain.invoke({
+    "topic":"AI의 미래는?"
+})
 
-# print(result)
+print(result)
--- 09.RAG-랭체인-랭그래프/03.랭체인/03-랭체인-구조화_출력.py
+++ 09.RAG-랭체인-랭그래프/03.랭체인/03-랭체인-구조화_출력.py
@@ -33,12 +33,3 @@ print("직업 :",result.job)
 
 print(result.model_dump())
 # {'name': '김철수', 'age': 25, 'job': '개발자'} 와 같이 Json 형식으로 출력# 첫번째 질문
-
-response = llm.invoke("안녕하세요, 제이름은 홍길동 입니다.")
-print("응답1", response.content)
-# 응답 안녕하세요, 홍길동님! 만나서 반갑습니다. 어떻게 도와드릴까요?
-
-# 두번째 질문
-response = llm.invoke("제 이름은 뭐였죠")
-print("응답2", response.content)
-# 답2 죄송하지만, 당신의 이름을 알 수 있는 정보가 없습니다. 당신의 이름을 알려주시면 그에 맞춰 대화할 수 있습니다!
