e4bcc93c49a2766a7dfd36cc9d085d07a88ed9a0
admin <admin@admin.com>
2026-07-08 08:54:03 +0900

    commit

--- 09.RAG-랭체인-랭그래프/04.메모리관리/03-메모리관리-기본-저장하기.py
+++ 09.RAG-랭체인-랭그래프/04.메모리관리/03-메모리관리-기본-저장하기.py
@@ -1,3 +1,4 @@
+
 from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
 from langchain_core.prompts import ChatPromptTemplate
@@ -7,11 +8,10 @@ import os
 from langchain_core.prompts import MessagesPlaceholder
 from langchain_core.messages import HumanMessage, AIMessage
 
-
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 from llm_loader import init_custom_llm
 
-#============================================================
+#==================================================
 # 예제 1. 리스트로 대화 저장하기 (Memory가 없을 때)
 history = []
 
@@ -22,6 +22,7 @@ history.append("사용자 : 제 이름은 철수입니다.")
 history.append("AI : 반갑습니다 철수님.")
 
 print(history)
+
 
 # 문자열만 저장하면 AI가 이해할 수 있을까요?"
 # AI는 누가 말했는지(Human/AI)를 알아야 함
@@ -39,12 +40,15 @@ history.append(AIMessage("반갑습니다 철수님."))
 
 print(history)
 
-# ChatMessageHistory 등장
-from langchain_community.chat_message_histories import ChatMessageHistory
+#pip install langchain langchain-community
 
-history = ChatMessageHistory()  # 메모리 관리하는 함수 ChatMessageHistory, InMemoryChatMessageHistory, RunnableWithMessageHistory
+from langchain_community.chat_message_histories import ChatMessageHistory
+# ChatMessageHistory 등장
+
+history = ChatMessageHistory() # =>InMemoryChatMessageHistory ,RunnableWithMessageHistory
+
 history.add_user_message("안녕하세요")
-history.add_ai_message("무엇을 도와드릴까요?")
+history.add_ai_message("무엇을 도와 드릴까요")
 
 print(history.messages)
 
@@ -53,11 +57,54 @@ for message in history.messages:
     print(message.content)
 
 # [
-#     HumanMessage(content="안녕하세요",additinal_kwargs={},response_metadata={}),
-#     AIMessage(content="무엇을 도와드릴까요?",additinal_kwargs={},response_metadata={}, tool_calls={}, invalid_tool_calls=[])
+#     HumanMessage(content='안녕하세요', additional_kwargs={}, response_metadata={}), 
+#     AIMessage(content='무엇을 도와 드릴까요', additional_kwargs={}, response_metadata={}, tool_calls=[], invalid_tool_calls=[])
 # ]
 
-# 템플릿 사용
 
+# 템플릿 사용
+from langchain_core.chat_history import InMemoryChatMessageHistory
+
+history = InMemoryChatMessageHistory() # 메모리 관리하는 함수 ChatMessageHistory, InMemoryChatMessageHistory, RunnableWithMessageHistory
+
+history.add_user_message("안녕하세요")
+history.add_ai_message("무엇을 도와 드릴까요")
+
+history.add_user_message("제이름은 철수 입니다.")
+history.add_ai_message("반갑습니다 철수님")
+
+print(history.messages[0].content)
+print(history.messages[1].content)
+print(history.messages[2].content)
+print(history.messages[3].content)
+
+history.clear()
+print("내용",history.messages)
+
+# print(history.messages)
+
+# for message in history.messages:
+#     print(type(message))
+#     print(message.content)
+
+# LLM 과 연결하기 예제
+history = InMemoryChatMessageHistory()
+# history = [
+# HumanMessage(content='내 이름은 철수야', additional_kwargs={}, response_metadata={}), 
+# ]
 llm = init_custom_llm()
 
+question = "내 이름은 철수야"
+
+history.add_user_message(question)
+response = llm.invoke(history.messages)
+print(response.content)
+history.add_ai_message(response.content)
+
+# 두번째 질문
+question = "내 이름이 뭐지?"
+
+history.add_user_message(question)
+response = llm.invoke(history.messages)
+print(response.content)
+history.add_ai_message(response.content)
---
+++ 09.RAG-랭체인-랭그래프/04.메모리관리/05.메모리관리-DB-저장.py
@@ -0,0 +1,57 @@
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai import ChatOpenAI
+
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+from llm_loader import init_custom_llm
+
+llm = init_custom_llm()
+
+# 2. Prompt 구성 (history + question 구조)
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "당신은 친절한 AI 비서입니다."),
+    ("placeholder", "{history}"),
+    ("human", "{question}")
+])
+
+# 3. 기본 chain 생성
+chain = prompt | llm
+
+from langchain_community.chat_message_histories import SQLChatMessageHistory
+# MySQL 연결
+def get_sql_session_history(session_id: str):
+
+    return SQLChatMessageHistory(
+        session_id=session_id,
+        connection="mysql+pymysql://scott:tiger@localhost:3306/scott",
+        table_name="message_store" # DB에 생성될 테이블 이름 (기본값: message_store)
+    )
+
+chain_with_sql_history = RunnableWithMessageHistory(
+    chain,
+    get_sql_session_history,
+    input_messages_key="question",
+    history_messages_key="history",
+)
+
+while True:
+
+    q = input("질문: ")
+
+    if q == "exit":
+        break
+
+    res = chain_with_sql_history.invoke(
+        {"question": q},
+        config={
+            "configurable": {
+                "session_id": "user_1"
+            }
+        }
+    )
+
+    print("\nAI:", res.content)
