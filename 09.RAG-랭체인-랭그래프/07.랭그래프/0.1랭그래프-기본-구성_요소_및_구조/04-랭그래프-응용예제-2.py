11dded42716a4acdca0fa27656188dcea7f22934
admin <admin@admin.com>
2026-07-14 10:11:52 +0900

    commit

---
+++ 09.RAG-랭체인-랭그래프/07.랭그래프/0.1랭그래프-기본-구성_요소_및_구조/04-랭그래프-응용예제-2.py
@@ -0,0 +1,50 @@
+from typing import TypedDict
+from langgraph.graph import StateGraph, START, END
+
+# 1. state 정의
+class State(TypedDict):
+    price:int
+
+# 2. node 함수
+
+def discount(state:State):
+    state["price"] = int(state["price"] * 0.9)
+    return state
+
+def delivery(state:State):
+    state["price"] = state["price"] + 5000
+    return{
+        "price" : state["price"] + 5000
+    }
+
+# 3. 그래프 생성
+builder = StateGraph(State)
+
+builder.add_node("discount",discount)
+builder.add_node("delivery",delivery)
+
+# START
+#   ↓
+# discount
+#   ↓
+# delivery
+#   ↓
+# END
+# 초기값
+# 100000
+# 결과
+# 95000
+
+# 계산 과정
+
+# 100000
+# ↓
+
+# 10% 할인
+
+# 90000
+# ↓
+
+# 배송비 +5000
+
+# 95000
