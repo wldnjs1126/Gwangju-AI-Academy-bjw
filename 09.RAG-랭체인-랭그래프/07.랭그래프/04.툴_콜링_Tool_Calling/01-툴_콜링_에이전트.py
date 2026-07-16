497843214e4fe2b3c56e91ddaf86b318b0beec71
admin <admin@admin.com>
2026-07-16 11:31:18 +0900

    commit

--- 09.RAG-랭체인-랭그래프/07.랭그래프/04.툴_콜링_Tool_Calling/01-툴_콜링_에이전트.py
+++ 09.RAG-랭체인-랭그래프/07.랭그래프/04.툴_콜링_Tool_Calling/01-툴_콜링_에이전트.py
@@ -29,8 +29,39 @@ from  datetime import datetime
 def age(birth_year):
     """
     출생연도를 입력받아 현재 나이를 계산합니다.
-    """
-    
+    """    
     current_year = datetime.now().year
     
     return current_year - birth_year
+
+result = age.invoke(
+    {
+        "birth_year":2000
+    }
+)
+print(result)
+print(age.name)
+print(age.description)
+
+from langchain.agents import create_agent
+
+agent = create_agent(
+    model=llm,
+    tools=[age],
+    system_prompt="""
+    당신은 친절한 AI 비서입니다.
+
+    필요하면 Tool을 사용하여 답변하세요.
+    """
+)
+from langchain_core.messages import  HumanMessage, SystemMessage
+
+result = agent.invoke(
+    {
+        "messages":[HumanMessage(content="2000년생 나이는?")]
+    }
+)
+
+print(result["messages"][-1].content)
+print("="*50)
+print(result)
